0418c54102ef6313669a7b50577ee862fa9ab53b
William Morgan <sghp78@durham.ac.uk>
2025-08-06 17:49:44 +0100

    Made into two pointer approach

--- Python Solutions/1768-MergeStringsAlternately.py
+++ Python Solutions/1768-MergeStringsAlternately.py
@@ -8,21 +8,41 @@ class Solution(object):
         
         # First approach
 
+        # output = ""
+
+        # while word1 != "" and word2 != "":
+        #     # Add first character of strings to output
+        #     output += word1[0]
+        #     output += word2[0]
+
+        #     # Use slicing to remove first character
+
+        #     word1 = word1[1:]
+        #     word2 = word2[1:]
+
+        # # Append both strings to end, since one should be empty
+
+        # output += word1
+        # output += word2
+
+
+
+        # Let's try a two pointer approach
+
+        m = len(word1)
+        n = len(word2)
+
         output = ""
 
-        while word1 != "" and word2 != "":
-            # Add first character of strings to output
-            output += word1[0]
-            output += word2[0]
+        i = 0
+        j = 0
 
-            # Use slicing to remove first character
-
-            word1 = word1[1:]
-            word2 = word2[1:]
-
-        # Append both strings to end, since one should be empty
-
-        output += word1
-        output += word2
+        while i < m or j < n:
+            if i < m:
+                output += word1[i]
+                i+=1
+            if j < n:
+                output += word2[j]
+                j+=1
 
         return output
