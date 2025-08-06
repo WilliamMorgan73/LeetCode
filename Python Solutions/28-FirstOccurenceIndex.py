175b09200296bfde73848de457e06dfca6cd027d
William Morgan <sghp78@durham.ac.uk>
2025-08-06 20:45:15 +0100

    Question 28 complete

---
+++ Python Solutions/28-FirstOccurenceIndex.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+
+        count = 0
+        j = 0
+        prevIndex = 0
+        i = 0
+
+        while i <  len(haystack):
+            if haystack[i] == needle[j]:
+                # Check if start of word, if so remember the index of the next character for backtracking
+                if j == 0:
+                    prevIndex = i+1
+                
+                if count == (len(needle)-1):
+                    return (i - count)
+                
+                # Increment count
+                count += 1
+
+                # Increment pointers
+                i += 1
+                j +=1
+                
+            else:
+
+                # Check if count > 0
+                if count > 0:
+                    # Roll back
+                    i = prevIndex
+                else:
+                    # Next char
+                    i +=1
+
+                # Increment pointer, reset count
+                j=0
+                count = 0
+
+        return -1    
